93bf6952fe7b866a6238c5d73e27fcab6895437b
imaharu <light1200out@gmail.com>
2018-05-28 04:31:14 +0900

    Create databases

---
+++ database/migrations/2018_05_27_193009_create_tags_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('place');  // 配置場所
+            $table->string('symbol'); // 請求記号
+            $table->integer('tag1');
+            $table->integer('tag2');
+            $table->integer('tag3');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
