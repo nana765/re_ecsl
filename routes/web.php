d3326461f15a9ab00521d23c3253e0eb20911d40
imaharu <light1200out@gmail.com>
2018-05-28 06:22:39 +0900

    Fix routing

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,12 @@
 Route::view('/', 'index');
 Route::view('/register', 'register');
 
-Route::resource('/register/book', 'BookController', ['only' => [
-    'create', 'store'
-]]);
+Route::get('/book', 'BookController@index');
+Route::get('/register/book', 'BookController@register');
+Route::post('/register/book', 'BookController@store');
 
-Route::resource('/register/tag', 'TagController', ['only' => [
-    'create', 'store'
-]]);
+Route::get('/register/tag', 'TagController@register');
+Route::post('/register/tag', 'TagController@store');
 
 Route::resource('/admin/book', 'BookController', ['only' => [
     'index', 'show', 'delete'
