410bbf0e051baeb0ebe689542400001d57fece2c
imaharu <light1200out@gmail.com>
2018-06-09 17:11:01 +0900

    文言追加

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,7 +43,7 @@
                             <div class="col-md-6 offset-md-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('') }}
+                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('次回から覚える') }}
                                     </label>
                                 </div>
                             </div>
