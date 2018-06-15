e56cd9205d5b9a4dc451492e424362eb3dfa1e98
nana <nana.okada7@gmail.com>
2018-06-15 23:19:40 +0900

    ISBNのエラー処理
    同じISBNが登録されている場合エラーを表示させる

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -7,6 +7,7 @@ use App\Book;
 use App\Tag;
 use Validator;
 
+
 class BookController extends Controller
 {
 
@@ -14,7 +15,7 @@ class BookController extends Controller
     {
         $books = Book::where('set',false)->paginate(15);;
         if(empty($keyword)){
-            
+
         }
         return view('book.index',compact('books'));
     }
@@ -31,6 +32,7 @@ class BookController extends Controller
             'name' => 'required|max:128',
             'place' => 'required|max:128',
             'symbol' => 'required|max:128',
+            'isbn' =>'required|unique:books,isbn|digits:13',
             'tag1' => 'required|max:5',
             'tag2' => 'required|max:5',
             'tag3' => 'required|max:5',
@@ -46,6 +48,7 @@ class BookController extends Controller
         $book->name = $request->name;
         $book->place = $request->place; //  配置場所
         $book->symbol = $request->symbol; //  請求記号
+        $book->isbn = $request->isbn; //ISBNコード
         $book->tag1 = $request->tag1;
         $book->tag2 = $request->tag2;
         $book->tag3 = $request->tag3;
