6ada3c35d84ff0c0620b2b9a03a46c4e4102e708
imaharu <light1200out@gmail.com>
2018-06-21 00:47:05 +0900

    Update register.blade.php

--- resources/views/tag/register.blade.php
+++ resources/views/tag/register.blade.php
@@ -18,13 +18,11 @@
 
 <div class="container pt-50">
   {!! Form::open(['url' => 'register/tag']) !!}
-  <!--バリデーションエラー表示-->
+  
   @if (count($errors) > 0)
-  <!-- フォームのエラーリスト -->
-  <div class="alert alert-danger">
-    <strong>すでに登録されています</strong>
-    <br>
-  </div>
+      <div class="alert alert-danger">
+        <strong>すでに登録されています</strong>
+      </div>
   @endif
 
   <div class="form-group">
